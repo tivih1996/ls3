553f99aa7aebb3f44416f0281aa58d5782ef05a6
Your Name <you@example.com>
2024-07-11 16:04:20 +0700

    update

---
+++ src/components(rfce)/card.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import '../App.css';
+
+function card() {
+  return (
+    <>
+    <div className="container">
+      <div className="item">1</div>
+      <div className="item">2</div>
+      <div className="item">3</div>
+      <div className="item">4</div>
+      <div className="item">5</div>
+      <div className="item">6</div>
+    </div>
+    
+    
+    </>
+
+
+
+  )
+}
+
+export default card;
